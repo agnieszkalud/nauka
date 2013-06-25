56993b3bad83b25727cd363217b28b8da4a34a59
Aga <agnieszkalud@op.pl>
2013-06-25 17:25:52 +0200

    próba

---
+++ src/pl/wazniak/Data.java
@@ -0,0 +1,17 @@
+package pl.wazniak;
+
+public class Data {
+	private int dzien;
+	private int miesiac;
+	private int rok;
+
+	public Data(int dzien, int miesiac, int rok) {
+		this.dzien = dzien;
+		this.miesiac = miesiac;
+		this.rok = rok;
+	}
+
+	public String pokazDate(){
+		return dzien + "." + miesiac + "." + rok;
+ }
+}
---
+++ src/pl/wazniak/DataTest.java
@@ -0,0 +1,9 @@
+package pl.wazniak;
+
+public class DataTest {
+
+	public static void main(String[] args) {
+		Data data = new Data(14, 12, 1993);
+		System.out.println(data.pokazDate());
+	}
+}
