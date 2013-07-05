d6b9777a87680d380c41481d5732ceb26e3bc0e7
Aga <agnieszkalud@op.pl>
2013-07-05 09:51:22 +0200

    Poprawiony komentarz

--- src/algorytmy/info/RekurencjaFibbonacciegoTest.java
+++ src/algorytmy/info/RekurencjaFibbonacciegoTest.java
@@ -4,16 +4,18 @@ public class RekurencjaFibbonacciegoTest {
 
 	public static void main(String[] args) {
 		/**
-		 * metoda rekurencyjna wysypuje się przy 45 i większej liczbie
+		 * metoda rekurencyjna od 47 zwrca liczbę ujemną
 		 */
 		int wynik = RekurencjaFibbonacciego.rekurencjaFibbonacciego(5);
 		System.out.println("Wartość ciągu Fibbonacciego dla liczby 5 wynosi " + wynik);
 		/**
-		 * metoda ta (nie rekurencyjna) działa nawet przy 1147000600 następnie
-		 * gdzieś tam od pewnego momentu zwaraca wartości ujemne a póżniej przy
-		 * jeszcze większej liczbie zwraca błąd
+		 * foramlnie zakres int wynosi −2 147 483 648 — +2 147 483 647 (ze
+		 * znakiem, w tym programie przy 2 147 483 646 wynikami są już ujemne
+		 * warotości, natomiast przy 2147483648 i większej liczbie następuje
+		 * wyjątek związany z przekroczeniem zakresu:
+		 * "The literal 2147483648 of type int is out of range"
 		 */
-		int wynik2 = RekurencjaFibbonacciego2.rekurencjaFibbonacciego2(1147000600);
+		int wynik2 = RekurencjaFibbonacciego2.rekurencjaFibbonacciego2(214);
 		System.out.println("Wartość ciągu Fibbonacciego dla liczby 5 wynosi " + wynik2);
 	}
 }
