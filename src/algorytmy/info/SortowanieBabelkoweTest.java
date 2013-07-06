939ca91eac8c7e9a1d89f76cb740db679a28f700
Aga <agnieszkalud@op.pl>
2013-07-06 10:41:52 +0200

    Sortowanie Babelkowe

--- src/algorytmy/info/SortowanieBabelkoweTest.java
+++ src/algorytmy/info/SortowanieBabelkoweTest.java
@@ -3,6 +3,7 @@ package algorytmy.info;
 public class SortowanieBabelkoweTest {
 
 	public static void main(String[] args) {
+
 		SortowanieBabelkowe wynik = new SortowanieBabelkowe(7);
 
 		wynik.wydrukujTablice();
