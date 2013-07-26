8288f305311eceef9dc759ed41a600e1bf775bb0
Aga <agnieszkalud@op.pl>
2013-07-26 21:51:46 +0200

    Stos z wykorzystaniem tablicy

---
+++ src/algorytmy/info/Stos.java
@@ -0,0 +1,76 @@
+package algorytmy.info;
+
+/**
+ * stos (ang. stack) - umożliwia dostęp tylko do jednego lementu - ostatniego,
+ * LIFO (Last-In-First-Out,"ostatni wszedł, pierwszy wyszedł" );
+ * 
+ */
+public class Stos {
+
+	private int maxRozmiar;
+	private int[] tabStosu;
+	private int szczyt;// ang. top
+
+	/**
+	 * konstruktor - ustawia rozmiar tablicy, tworzy tablice, inkrementuje
+	 * zmienna wskazujaca na element na stosie
+	 * 
+	 * @param s
+	 */
+	public Stos(int s) {
+		maxRozmiar = s;
+		tabStosu = new int[maxRozmiar];
+		szczyt = -1;// na razie brak elementow
+	}
+
+	/**
+	 * metoda odluz(ang. push)do odkladania elementow na szczyt stosu
+	 */
+	public void odloz(int j) {
+		tabStosu[++szczyt] = j;// zwiekszam szyt(ang. top), odkladam element
+	}
+
+	/**
+	 * zdejmij(ang. pop) pobiera element ze szytu stosu
+	 */
+	public int zdejmij() {
+		return tabStosu[szczyt--];// pobieram element, zmniejszam szczyt
+	}
+
+	/**
+	 * podejrzyj(ang. peek) podglada wartosc na szczycie stosu
+	 * 
+	 * @return
+	 */
+	public int podejrzyj() {
+		return tabStosu[szczyt];
+	}
+
+	/**
+	 * zwraca true jesli stos pusty
+	 * 
+	 * @return
+	 */
+	public boolean jestPusty() {
+		return (szczyt == -1);
+	}
+
+	public boolean jestPelny() {
+		return (szczyt == maxRozmiar - 1);
+	}
+
+	public void zobaczSklad() {
+		int pomocnicza = szczyt;
+		while (pomocnicza != -1) {
+			System.out.print(tabStosu[pomocnicza] + " ");
+			pomocnicza--;
+		}
+		System.out.println("");
+	}
+
+	public void zdejmijWszystko() {
+		while (!jestPusty()) {
+			zdejmij();
+		}
+	}
+}
---
+++ src/algorytmy/info/StosTest.java
@@ -0,0 +1,20 @@
+package algorytmy.info;
+
+public class StosTest {
+
+	public static void main(String[] args) {
+		Stos stos = new Stos(5); // kstos piecioelementowy
+
+		stos.jestPelny();
+
+		stos.odloz(10);
+		stos.odloz(20);
+		stos.odloz(30);
+		stos.odloz(40);
+
+		stos.zobaczSklad();
+
+		stos.zdejmijWszystko();
+	}
+}
+
