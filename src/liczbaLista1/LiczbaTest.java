772e80339423d77efdd21e6f473cf5b13c446b01
Aga <agnieszkalud@op.pl>
2013-10-05 20:33:17 +0200

    asercje dodane

--- src/liczbaLista1/LiczbaTest.java
+++ src/liczbaLista1/LiczbaTest.java
@@ -1,5 +1,11 @@
 package liczbaLista1;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -22,115 +28,130 @@ public class LiczbaTest {
 	}
 
 	@Test(expected = LiczbaException.class)
-	public void testujWyjatek() throws LiczbaException {
+	public final void testPoprawnoscWyjatkuJesliTworzonyJestObiektDlaDlaNieprawidlowegoParametru() throws LiczbaException {
 		int nieprawidłowaLiczba = -1;
-		liczba = new Liczba(nieprawidłowaLiczba);
+		try {
+			liczba = new Liczba(nieprawidłowaLiczba);
+		} catch (Exception e) {
+			// poszedl wyjatek czyli to czego oczekujemy
+			if (!(e instanceof LiczbaException)) {
+				fail("Konstruktor rzucił nieoczekiwany wyjatek");
+			}
+		}
+	}
+
+	@Test
+	public final void testPoprawnoscUtowrzenieObiektuLiczbyDlaPrawidlowegoParametru() throws LiczbaException {
+		int proprawnaLiczbaDoKonwersji = 123;
+		liczba = new Liczba(proprawnaLiczbaDoKonwersji);
+		assertNotNull(liczba);
 	}
 
 	@Ignore
 	public final void testKonwersjaNaSzstemBinarny() {
 		int system = 2;
 		String wynikKonwersji = liczba.konwersjaNaInnySystem(system);
-		wynikKonwersji.equals("10000");
+		assertTrue(wynikKonwersji.equals("10000"));
 	}
 
 
-	@Test(timeout = 999999999)
-	public final void testKonwersjaNaSzstemTrójkowy() {
-		int system = 16;
+	@Test(timeout = 999)
+	public final void testKonwersjaNaSzstemTrójkowyDlaBardzoDuzejLiczby() throws LiczbaException {
+		liczba = new Liczba(Integer.MAX_VALUE);
+		int system = 2;
 		String wynikKonwersji = liczba.konwersjaNaInnySystem(system);
-		wynikKonwersji.equals("121");
+		assertEquals("1111111111111111111111111111111", wynikKonwersji);
 	}
 
 	@Ignore
 	public final void testKonwersjaNaSzstemCzworkowy() {
 		int system = 4;
 		String wynikKonwersji = liczba.konwersjaNaInnySystem(system);// odp.100
-		wynikKonwersji.equals("24");
+		assertFalse(wynikKonwersji.equals("24"));
 	}
 
 	@Test
 	public final void testKonwersjaNaSzstemPiatkowy() {
 		int system = 5;
 		String wynikKonwersji = liczba.konwersjaNaInnySystem(system);
-		wynikKonwersji.equals("31");
+		assertEquals("31", wynikKonwersji);
 	}
 
 	@Test
 	public final void testKonwersjiNaSystemSzostkowy() {
 		int system = 6;
 		String wynikKonwersji = liczba.konwersjaNaInnySystem(system);// odp.24
-		wynikKonwersji.equals("23");
-	}// TODO nie zglasza bledu?
+		assertFalse(wynikKonwersji.equals("23"));
+	}
 
 	@Test
 	public final void testKonwersjaNaSzstemSiodemkowy() {
 		int system = 7;
 		String wynikKonwersji = liczba.konwersjaNaInnySystem(system);
-		wynikKonwersji.equals("22");
+		assertTrue(wynikKonwersji.equals("22"));
 	}
 
 	@Test
 	public final void testKonwersjaNaSzstemOsemkowy() {
 		int system = 8;
 		String wynikKonwersji = liczba.konwersjaNaInnySystem(system);
-		wynikKonwersji.equals("20");
+		assertTrue(wynikKonwersji.equals("20"));
 	}
 
 	@Test
 	public final void testKonwersjaNaSzstemDziewiatkowy() {
 		int system = 9;
 		String wynikKonwersji = liczba.konwersjaNaInnySystem(system);
-		wynikKonwersji.equals("17");
+		assertEquals("17", wynikKonwersji);
 	}
 
 	@Test
 	public final void testKonwersjaNaSzstemDziesiatkowy() {
 		int system = 10;
 		String wynikKonwersji = liczba.konwersjaNaInnySystem(system);
-		wynikKonwersji.equals("16");
+		assertTrue(wynikKonwersji.equals("16"));
 	}
 
 	@Test
 	public final void testKonwersjaNaSzstemJedenastkowy() {
 		int system = 11;
 		String wynikKonwersji = liczba.konwersjaNaInnySystem(system);
-		wynikKonwersji.equals("15");
+		assertTrue(wynikKonwersji.equals("15"));
 	}
 
 	@Test
 	public final void testKonwersjaNaSzstemDwunastkowy() {
 		int system = 12;
 		String wynikKonwersji = liczba.konwersjaNaInnySystem(system);
-		wynikKonwersji.equals("14");
+		assertEquals("14", wynikKonwersji);
 	}
 
 	@Test
 	public final void testKonwersjaNaSzstemTrzynastkowy() {
 		int system = 13;
 		String wynikKonwersji = liczba.konwersjaNaInnySystem(system);
-		wynikKonwersji.equals("13");
+		assertFalse(wynikKonwersji.equals("10"));
 	}
 
 	@Test
 	public final void testKonwersjaNaSzstemCznernastkowy() {
 		int system = 14;
-		String wynikKonwersji = liczba.konwersjaNaInnySystem(system);
-		wynikKonwersji.equals("12");
+		String wynikKonwersji = liczba.konwersjaNaInnySystem(system);// odp.12
+		assertFalse(wynikKonwersji.equals("10"));
 	}
 
 	@Test
 	public final void testKonwersjaNaSzstemPietnastkowy() {
 		int system = 15;
 		String wynikKonwersji = liczba.konwersjaNaInnySystem(system);
-		wynikKonwersji.equals("11");
+		assertEquals("11", wynikKonwersji);
 	}
 
 	@Test
 	public final void testKonwersjaNaSzstemSzesnastkowy() {
 		int system = 16;
 		String wynikKonwersji = liczba.konwersjaNaInnySystem(system);
-		wynikKonwersji.equals("10");
+		assertEquals("10", wynikKonwersji);
 	}
 
 
